d4a06c632cd41fb412fe9e7f1e55cd9ddd3b1157
codecos <kai199529@hotmail.com>
2018-04-16 14:06:31 +0800

    init

---
+++ MQReceive/Program.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MQReceive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() {
+                HostName = MQConfig.HostName,
+                UserName = MQConfig.UserName,
+                Password = MQConfig.Password,
+            };
+
+            Receive.ReceiveData(factory);
+
+        }
+    }
+}
